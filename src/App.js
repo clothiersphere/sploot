5e41cead4bc5130de358b7e01bca22ec26b25c03
Christopher Lee <xchrislee@gmail.com>
2018-07-01 21:17:03 -0700

    Run eject to access to config to modify entry point

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
+import { Input, Button } from 'semantic-ui-react';
 import logo from './logo.svg';
 import './App.css';
-import { Input } from 'semantic-ui-react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      link: '',
+      key: '',
+      megalink: '',
+      decryptionKey: '',
+    };
+  }
+
+  // <img src={logo} className="App-logo" alt="logo" />
+
   render() {
+    const { megalink, decryptionKey } = this.state;
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">
-Welcome to React
+            Sploot Megalink Utility Tool
           </h1>
         </header>
-        <Input placeholder="megalink" />
-        <Input placeholder="decryption key" />
+        <Input placeholder="megalink" value={megalink} />
+        <Input placeholder="decryption key" value={decryptionKey} />
+        <Button>
+          generate link
+        </Button>
       </div>
     );
   }
